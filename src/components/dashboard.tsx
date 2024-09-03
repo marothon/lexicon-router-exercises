aa3319cbf44c8c17c20345aa4ca20bb9afc3a8aa
marothon <marothon@gmail.com>
2024-09-03 10:13:26 +0200

    Solve task 6

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,12 +1,25 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+
+interface LinkState {
+  isActive: boolean,
+  isPending: boolean,
+  isTransitioning: boolean
+}
 
 export default function Dashboard() {
+  const handleLinkStateClass = ({ isActive, isPending, isTransitioning }: LinkState) =>
+    [
+      isPending ? "pending" : "",
+      isActive ? "active" : "",
+      isTransitioning ? "transitioning" : "",
+    ].join(" ");
+
   return (
     <section className="dashboard">
       <h1>Dashboard <aside className='material-symbols-outlined'>dashboard</aside></h1>
       <nav>
-        <Link to='stats'>Stats</Link>
-        <Link to='settings'>Settings</Link>
+        <NavLink className={handleLinkStateClass} to='stats'>Stats</NavLink>
+        <NavLink className={handleLinkStateClass} to='settings'>Settings</NavLink>
       </nav>
       <Outlet/>
     </section>  
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,28 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+
+interface LinkState {
+  isActive: boolean,
+  isPending: boolean,
+  isTransitioning: boolean
+}
 
 export default function Header() {
+
+  const handleLinkStateClass = ({ isActive, isPending, isTransitioning }: LinkState) =>
+    [
+      isPending ? "pending" : "",
+      isActive ? "active" : "",
+      isTransitioning ? "transitioning" : "",
+    ].join(" ");
+  
+
   return (
     <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/contact'>Contact</Link>
-      <Link to='/dashboard'>Dashboard</Link>
-      <Link to='/user/1'>Profile</Link>
+      <NavLink className={handleLinkStateClass} to='/'>Home</NavLink>
+      <NavLink className={handleLinkStateClass} to='/about'>About</NavLink>
+      <NavLink className={handleLinkStateClass} to='/contact'>Contact</NavLink>
+      <NavLink className={handleLinkStateClass} to='/dashboard'>Dashboard</NavLink>
+      <NavLink className={handleLinkStateClass} to='/user/1'>Profile</NavLink>
     </nav>
   )
 }
